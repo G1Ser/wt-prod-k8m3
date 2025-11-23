dd7240a0e8c729a84a04432fc3a12c6c6abe2162
Chauncey <chauncey1129@163.com>
2025-11-23 18:45:27 +0800

    fix：修改域名

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -3,5 +3,5 @@ const MINUTE = 60;
 const HOUR = 60 * MINUTE;
 const DAY = 24 * HOUR;
 // CORS 白名单
-const CORS_ORIGINS = ["https://weatherv1-chauncey.work"];
+const CORS_ORIGINS = ["https://app.chauncey.work"];
 export { MINUTE, HOUR, DAY, CORS_ORIGINS };
