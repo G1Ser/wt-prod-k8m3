a205cb3fd4c66377113ca3a0e5e4a801bbb6e8f8
Chauncey <chauncey1129@163.com>
2025-11-25 21:04:32 +0800

    fix：增加白名单

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -3,5 +3,10 @@ const MINUTE = 60;
 const HOUR = 60 * MINUTE;
 const DAY = 24 * HOUR;
 // CORS 白名单
-const CORS_ORIGINS = ["https://weatherv1.chauncey.work"];
+const CORS_ORIGINS = [
+  "https://weatherv1.chauncey.work",
+  "https://weatherv2.chauncey.work",
+  "https://weatherv3.chauncey.work",
+  "https://weatherv4.chauncey.work",
+];
 export { MINUTE, HOUR, DAY, CORS_ORIGINS };
