4afd8b0e08e5dbcc0822e50a233d00a635f2226b
Chauncey <chauncey0420zx@gmail.com>
2026-03-25 11:10:43 +0800

    feat: 重新生成IP、Geo接口文档

--- handlers/geo.js
+++ handlers/geo.js
@@ -1,8 +1,31 @@
 /**
  * @openapi
- * /geo/search:
+ * /geo/adcode:
  *   get:
- *     summary: 地址位置搜索
+ *     summary: 地址位置搜索（返回adcode，仅限中国区域）
+ *     tags: [Geo]
+ *     parameters:
+ *       - in: query
+ *         name: keyword
+ *         required: true
+ *         schema:
+ *           type: string
+ *           example: "北京"
+ *         description: 查询的地址关键词，支持模糊搜索
+ *     responses:
+ *       200:
+ *         description: 成功返回地址位置信息
+ *       400:
+ *         description: 缺少必要参数
+ *       403:
+ *         description: 请求来源不在白名单
+ */
+
+/**
+ * @openapi
+ * /geo/coordinate:
+ *   get:
+ *     summary: 地址位置搜索（优先检索数据库数据，再匹配openstreetmap数据）
  *     tags: [Geo]
  *     parameters:
  *       - in: query
--- handlers/ip.js
+++ handlers/ip.js
@@ -4,6 +4,19 @@
  *   get:
  *     summary: 获取客户端 IP 地理定位信息
  *     tags: [IP]
+ *     parameters:
+ *       - in: query
+ *         name: lon
+ *         schema:
+ *           type: string
+ *           example: "116.6517"
+ *         description: 查询的经度
+ *       - in: query
+ *         name: lat
+ *         schema:
+ *           type: string
+ *           example: "39.9069"
+ *         description: 查询的维度
  *     responses:
  *       200:
  *         description: 成功返回 IP 定位数据
